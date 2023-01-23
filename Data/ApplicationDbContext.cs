185df548f0a5abdd9e4fb8ac2dea89eac7dab77b
kohdc <kohdc1723@gmail.com>
2023-01-23 00:36:08 -0800

    complete migrations and setup syncfusion

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
